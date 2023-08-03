6c0eede8c19381c23f15efecd703d1c60dbb1e3a
Gonzalo <molinagomezgonzalo@gmail.com>
2023-08-02 23:49:11 -0300

    feat: delivery-men component created

--- app/(pages)/driver-management/page.tsx
+++ app/(pages)/driver-management/page.tsx
@@ -5,6 +5,7 @@ import LayoutContainer from '../../layoutContainer'
 import Slider from 'react-slick'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
+import CircularProgressBar from 'common/CircularProgressBar'
 
 const usersData = [
     {

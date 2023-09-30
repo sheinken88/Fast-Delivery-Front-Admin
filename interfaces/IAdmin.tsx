4697adf1084d9acad7aa510c42d106278d0c7852
Gonzalo <molinagomezgonzalo@gmail.com>
2023-09-30 02:45:17 -0300

    bugfix: pictures updated

--- interfaces/IAdmin.tsx
+++ interfaces/IAdmin.tsx
@@ -1,7 +1,5 @@
 export default interface IAdmin {
-    _id: string
     username: string
     email: string
-    password: string
     profile_pic: string
 }
--- src/commons/ProfilePicture.tsx
+++ src/commons/ProfilePicture.tsx
@@ -1,13 +1,16 @@
 import Image from 'next/image'
+import { useSelector } from 'react-redux'
+import type { RootState } from 'store/store'
 
-export const ProfilePicture = (profileImg: string) => {
+export const ProfilePicture = () => {
+    const user = useSelector((state: RootState) => state.admin)
     return (
         <Image
             className="rounded-full"
             height={56}
             width={56}
             alt="Profile Picture"
-            src={profileImg}
+            src={user.profile_pic}
         />
     )
 }

216472c4da0db980d20629dbb2d279783bd8b9f5
Nahuel Venerus <nahuelvenerus@gmail.com>
2023-10-06 21:03:24 -0300

    fix: driver pictures work correctly and progress bar errors eliminated

--- src/components/DriverInfo.tsx
+++ src/components/DriverInfo.tsx
@@ -29,27 +29,29 @@ export const DriverInfo: React.FC<DriverInfoProps> = ({ driver, key }) => {
     }, [])
 
     return (
-        <div className="flex w-80 md:w-96 ">
+        <div className="flex w-80 md:w-96 p-2">
             <Link href={`/driver-details/${driver._id}`} key={driver._id}>
                 <div
                     className={`inset-0 ${
                         key === 0 || key % 4 === 0
                             ? 'border-t border-primary border-dashed'
                             : ''
-                    } flex items-center p-4`}
+                    } flex items-center m-2`}
                 >
-                    <div className="flex-shrink-0 w-24 h-24 flex justify-center items-center mr-4">
+                    <div className="flex-shrink-0 w-24 h-24 flex justify-center items-center mr-2">
                         <CircularProgressBar progress={progress} />
                     </div>
                     <div className="flex-grow">
-                        <h1 className="font-bold text-lg">{driver.username}</h1>
+                        <h1 className="flex font-bold text-lg">
+                            {driver.username}
+                        </h1>
                         <Tag
                             status={driver?.status}
                             value={driver?.status ? 'ACTIVE' : 'INACTIVE'}
                         />
                     </div>
 
-                    <div className="flex-shrink-0 w-14 h-14 rounded-full">
+                    <div className="flex-shrink-0 w-14 h-14 rounded-full mr-6">
                         <DriversPictures picture={driver.profile_pic} />
                     </div>
                 </div>
